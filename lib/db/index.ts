43e7b7e9df68414e52cac8e8db62350b6a142204
foasho <foa.sho0111@outlook.jp>
2024-02-24 12:20:46 +0900

    fix: URL

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,7 +1,7 @@
 import { drizzle, PostgresJsDatabase } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
-let DURL = process.env.DATABASE_URL as string | undefined;
+let DURL = process.env.POSTGRES_URL as string | undefined;
 // sslmode=require が必要
 if (!DURL) {
   throw new Error("POSTGRES_URL is not defined");
