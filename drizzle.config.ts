326c965140e4c8f9ef18f3cbd2653ed13760a948
foasho <foa.sho0111@outlook.jp>
2024-02-24 12:13:42 +0900

    sslmodeを修正

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,6 +5,7 @@ export default {
   driver: "pg",
   dbCredentials: {
     connectionString: process.env.POSTGRES_URL as string,
+    ssl: true,
   },
   verbose: true,
   strict: false, // pushする前に承認を求めない
