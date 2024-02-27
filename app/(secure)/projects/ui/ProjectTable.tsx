2d5e1f6edf07e43df8b38085d5dadd987cf019aa
foasho <foa.sho0111@outlook.jp>
2024-02-27 21:00:08 +0900

    fix: lint

--- app/(secure)/projects/ui/ProjectTable.tsx
+++ app/(secure)/projects/ui/ProjectTable.tsx
@@ -3,8 +3,6 @@ import { HiPlay, HiRocketLaunch } from "react-icons/hi2";
 import Link from "next/link";
 import { Button, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from "@nextui-org/react";
 
-import { ProjectData } from "@/db/types";
-
 import { InviteButton } from "./InviteButton";
 
 export const ProjectsTable = ({ projects, inviteUserInvitationAction }) => {
@@ -15,7 +13,7 @@ export const ProjectsTable = ({ projects, inviteUserInvitationAction }) => {
         <TableColumn>説明</TableColumn>
         <TableColumn>公開状態</TableColumn>
         <TableColumn>更新日</TableColumn>
-        <TableColumn className="min-w-48">操作</TableColumn>
+        <TableColumn className='min-w-48'>操作</TableColumn>
       </TableHeader>
       <TableBody>
         {projects.map((project) => (
@@ -24,7 +22,7 @@ export const ProjectsTable = ({ projects, inviteUserInvitationAction }) => {
             <TableCell>{project.description}</TableCell>
             <TableCell>{project.publish ? "公開中" : "非公開"}</TableCell>
             <TableCell>{project.updatedAt?.toString()}</TableCell>
-            <TableCell className="grid grid-cols-3 gap-4">
+            <TableCell className='grid grid-cols-3 gap-4'>
               <Button
                 target='_blank'
                 color='primary'
