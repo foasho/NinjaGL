57ec258dc28bb5cba12a1ea1a2585960429338bd
shoosaka <foa.sho0111@outlook.jp>
2024-03-31 12:45:26 +0900

    fix: update script

--- packages.update.sh
+++
@@ -1,4 +0,0 @@
-# pnpm installを行い、lockを更新後、docker-compose buildとupを行う
-pnpm install
-docker-compose build
-docker-compose up -d
---
+++ update.sh
@@ -0,0 +1,8 @@
+# @ninjagl/coreの最新版を取得
+pnpm install @ninjagl/core@latest
+
+# 依存パッケージをインストール
+pnpm install
+
+docker-compose build
+docker-compose up -d
